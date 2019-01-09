a1060c198cc7256d826400d847c44b0530121965
Jad Mrad <40995053+J-Mrad@users.noreply.github.com>
2019-01-10 00:33:22 +0200

    Added ability to pick which chapters to download

--- js/main.js
+++ js/main.js
@@ -28,6 +28,132 @@ function download(file){
 }
 
 
+
+
+function refresh(course){
+
+		courseSize = 0;
+		sumSize = 0;
+	
+		if(course==='i3301'){
+			courseSet = [725,771,242,2330,720,159,690,1662,443,327,274,307,601];
+			sumSet = [25,554,83,109,18,0,314,72,222,0,0,0,85];
+			
+			gitFlag = 0;
+			uml3Flag=0;
+
+			for(i = 1 ; i < 14 ; i++){	
+			  	if (document.getElementById(course+'.'+i).checked){
+			  		
+			  		if(i==5 || i ==6){
+			  			if(gitFlag==0){
+			  				gitFlag = 1;
+			  				sumSize += 18;
+			  			}
+			  		}
+			  		else if(i==9 || i ==10 || i==11 || i==12){
+			  			if(uml3Flag==0){
+			  				uml3Flag = 1;
+			  				sumSize += 222;
+			  			}
+			  		}
+			  		else{sumSize += sumSet[i-1];}
+
+			  		courseSize += courseSet[i-1];
+			  		
+		  		}
+		  	}
+	  	}
+
+		document.getElementById(course+'course').innerHTML = "Download course ("+courseSize+"KB)";
+		document.getElementById(course+'summary').innerHTML = "Download summary ("+sumSize+"KB)";
+		both = courseSize + sumSize;
+		document.getElementById(course+'both').innerHTML = "Download both ("+both+"KB)";
+}
+
+
+var Promise = window.Promise;
+if (!Promise) {
+    Promise = JSZip.external.Promise;
+}
+
+/**
+ * Fetch the content and return the associated promise.
+ * @param {String} url the url of the content to fetch.
+ * @return {Promise} the promise containing the data.
+ */
+function urlToPromise(url) {
+    return new Promise(function(resolve, reject) {
+        JSZipUtils.getBinaryContent(url, function (err, data) {
+            if(err) {
+                reject(err);
+            } else {
+                resolve(data);
+            }
+        });
+    });
+}
+
+
+
+function downloadnew(course, status){
+
+    var zip = new JSZip();
+    if(course==='i3301'){
+
+			courseSet = ['data/I3301/Courses/ch1',
+			'data/I3301/Courses/ch2',
+			'data/I3301/Courses/ch3',
+			'data/I3301/Courses/ch4a','data/I3301/Courses/ch4b',
+			'data/I3301/Courses/ch5a','data/I3301/Courses/ch5b',
+			'data/I3301/Courses/ch6a','data/I3301/Courses/ch6b','data/I3301/Courses/ch6c','data/I3301/Courses/ch6d','data/I3301/Courses/ch6e',
+			'data/I3301/Courses/ch7'];
+			sumSet = ['data/I3301/Summaries/ch1',
+			'data/I3301/Summaries/ch2',
+			'data/I3301/Summaries/ch3',
+			'data/I3301/Summaries/ch4',
+			'data/I3301/Summaries/git',null,
+			'data/I3301/Summaries/uml1',
+			'data/I3301/Summaries/uml2',
+			'data/I3301/Summaries/uml3',null,null,null,
+			'data/I3301/Summaries/ch7'];
+			
+			gitFlag = 0;
+			uml3Flag=0;
+    	for(i = 1 ; i < 14 ; i++){	
+		  	if (document.getElementById(course+'.'+i).checked){
+		  		
+		  		if(i==5 || i ==6){
+		  			if(gitFlag==0){
+		  				gitFlag = 1;
+						zip.file('ch'+i, urlToPromise('data/I3301/Summaries/git'), {binary:true});
+	 	 			}
+		  		}
+		  		else if(i==9 || i ==10 || i==11 || i==12){
+		  			if(uml3Flag==0){
+		  				uml3Flag = 1;
+						zip.file('ch'+i, urlToPromise('data/I3301/Summaries/uml3'), {binary:true});
+		  			}
+		  		}
+		  		else{
+		  		
+		  		}
+
+				zip.file('ch'+i, urlToPromise(courseSet[i-1]), {binary:true});
+
+	  		}
+	  	}
+
+	  	zip.generateAsync({type:"blob"})
+		.then(function(content) {
+    		// see FileSaver.js
+   	 		saveAs(content, "example.zip");
+		});
+ 	}
+
+
+}
+
 //Comment section
 
 var curComment = 1; //1 = light gray, 2 = mid gray
